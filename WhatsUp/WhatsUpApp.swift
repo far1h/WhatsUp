87a1b09baf6e050414f8a8b84d15a9891d05b932
far1h <farihmuhammad45@gmail.com>
2025-10-29 09:02:26 +0700

    Initial Commit

---
+++ WhatsUp/WhatsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsUpApp.swift
+//  WhatsUp
+//
+//  Created by Farih Muhammad on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
